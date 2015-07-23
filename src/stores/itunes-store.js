7b331f0a0fcf77962bfbee09dba122fde9a8935f
mike-ward <mike@mike-ward.net>
2015-07-23 10:29:42 -0400

    remove dumb es6 class stuff

--- src/stores/itunes-store.js
+++ src/stores/itunes-store.js
@@ -1,24 +1,17 @@
-import riot from 'riot'
-import jsonp from 'browser-jsonp'
+import riot from 'riot';
+import jsonp from 'browser-jsonp';
 
-class ITunesStore {
-  constructor() {
-    riot.observable(this);
-    this.bindEvents();
-  }
+(function () {
+  var iTunesStore = riot.observable();
 
-  bindEvents() {
-    this.on(riot.EVT.queryITunesStore, (term, media) => {
-      jsonp({
-        url: 'https://itunes.apple.com/search',
-        data: { term: term, media: media },
-        success: data => this.trigger(riot.EVT.queryITunesStoreSuccess, data),
-        error: err => alert(err)
-      });
+  iTunesStore.on(riot.EVT.queryITunesStore, (term, media) => {
+    jsonp({
+      url: 'https://itunes.apple.com/search',
+      data: { term: term, media: media },
+      success: data => iTunesStore.trigger(riot.EVT.queryITunesStoreSuccess, data),
+      error: err => alert(err)
     });
-  }
-}
+  });
 
-// add store to riot control
-let iTunesStore = new ITunesStore();
-riot.control.addStore(iTunesStore);
+  riot.control.addStore(iTunesStore);
+})();
